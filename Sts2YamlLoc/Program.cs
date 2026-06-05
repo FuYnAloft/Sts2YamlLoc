f5d5c56973435a68ae35061bf478b8be76d9d521
FuYnAloft <207443530+fuynaloft@users.noreply.github.com>
2026-06-05 23:54:58 +0800

    feat: 提供可用的 CLI

--- Sts2YamlLoc/Program.cs
+++ Sts2YamlLoc/Program.cs
@@ -1,16 +1,72 @@
 using Cocona;
+using Sts2YamlLoc.Formatters;
+using Sts2YamlLoc.Formatters.ModelId;
+using Sts2YamlLoc.IO;
+using Sts2YamlLoc.Models.Entries;
+using Sts2YamlLoc.Models.Loc;
+using Sts2YamlLoc.Pipeline.Mappers;
 
 var app = CoconaApp.Create();
 
-// Cocona 示例代码
-app.AddCommand("sync", (string path) =>
-{
-    Console.WriteLine($"正在同步路径: {path}");
-});
 
-app.AddCommand("export", (string format = "yaml") =>
+app.AddCommand("baselib", (
+    [Argument(Description = "输入的本地化根目录")] string inputDir,
+    [Argument(Description = "输出的本地化根目录")] string outputDir,
+    [Argument(Description = "命名空间的第一段")] string namespaceTop,
+    [Option(Description = "转换方向")] ConversionDirection direction = ConversionDirection.YamlToJson) =>
 {
-    Console.WriteLine($"导出格式: {format}");
+    switch (direction)
+    {
+        case ConversionDirection.YamlToJson:
+        {
+            var reader = new YamlLocBundleReader(inputDir);
+            var writer = new JsonLocBundleWriter(outputDir);
+            var bundleFormatters = new EntryMapper<NestedEntry, FlatEntry>(
+                new(
+                [
+                    "cards", "card_keywords", "enchantments", "events", "powers",
+                    "relics", "static_hover_tips", "characters", "monsters"
+                ], new BaseLibIdFormatter(namespaceTop)),
+                new(["gameplay_ui", "ui"], new DotFormatter()),
+                new(["ancients"], new BaseLibIdFormatter(namespaceTop, pos: 2))
+            );
+
+            LocBundle<NestedEntry>
+                .Create(reader)
+                .Pipe(bundleFormatters)
+                .Sink(writer);
+            return 0;
+        }
+        case ConversionDirection.JsonToYaml:
+        {
+            var reader = new JsonLocBundleReader(inputDir);
+            var writer = new YamlLocBundleWriter(outputDir);
+            var bundleFormatters = new EntryMapper<FlatEntry, NestedEntry>(
+                new(
+                [
+                    "cards", "card_keywords", "enchantments", "events", "powers",
+                    "relics", "static_hover_tips", "characters", "monsters"
+                ], new BaseLibIdFormatter(namespaceTop)),
+                new(["gameplay_ui", "ui"], new DotFormatter()),
+                new(["ancients"], new BaseLibIdFormatter(namespaceTop, pos: 2))
+            );
+
+            LocBundle<FlatEntry>
+                .Create(reader)
+                .Pipe(bundleFormatters)
+                .Sink(writer);
+            return 0;
+        }
+        default:
+            Console.Error.WriteLine("未知的转换方向。");
+            return 1;
+    }
 });
 
 app.Run();
+
+internal enum ConversionDirection
+{
+    YamlToJson,
+    JsonToYaml
+}
